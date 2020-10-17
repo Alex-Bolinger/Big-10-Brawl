6f38567726d80c9c8d6a60f9811eda39558c6cde
aboli3514 <39972127+aboli3514@users.noreply.github.com>
2020-10-17 12:58:27 -0400

    added Big10Brawl.iml

--- src/Big10Brawl.java
+++ src/Big10Brawl.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Big10Brawl {
     public static void main(String[] args) {
-        
+        Big10Brawl b10B = new Big10Brawl();
+        b10B.initFrame();
+    }
+
+    private JFrame welcome;
+
+    public Big10Brawl() {
+        welcome = new JFrame();
+    }
+
+    public void initFrame() {
+        welcome.setSize(new Dimension(640,360));
+        welcome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        welcome.setTitle("Big10Brawl");
+        welcome.add(new JComponent() {
+            public void paint(Graphics g) {
+
+            }
+        });
+        welcome.setVisible(true);
+        welcome.repaint();
     }
 }
