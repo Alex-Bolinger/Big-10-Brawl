cf6e4597817ded305c27ad9463724a6bad039f41
aboli3514 <39972127+aboli3514@users.noreply.github.com>
2020-10-17 16:35:05 -0400

    finished menu and made revisions to player

--- src/Big10Brawl.java
+++ src/Big10Brawl.java
@@ -1,6 +1,8 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.*;
 
@@ -12,41 +14,19 @@ public class Big10Brawl {
 
     private JFrame welcome;
     private JFrame waiting;
+    private JComponent welComp;
+
+    private Color startColor;
+    private Color startTextColor;
+    private Color joinColor;
+    private Color joinTextColor;
 
     public Big10Brawl() {
         welcome = new JFrame();
-    }
-
-    public void initFrame() {
-        welcome.setSize(1550,838);
-        welcome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        welcome.setTitle("Big Ten Brawl");
-        welcome.add(new JComponent() {
-            private Color startColor;
-            private Color startTextColor;
-            private Color joinColor;
-            private Color joinTextColor;
+        welComp = new JComponent() {
             private BufferedImage bg;
 
-            public void changeStartColor() {
-                if (startColor.equals(new Color(0,0,0))) {
-                    startColor = new Color(255,255,255);
-                    startTextColor = new Color(0,0,0);
-                } else {
-                    startColor = new Color(0,0,0);
-                    startTextColor = new Color(255,255,255);
-                }
-            }
 
-            public void changeJoinColor() {
-                if (joinColor.equals(new Color(0,0,0))) {
-                    joinColor = new Color(255,255,255);
-                    joinTextColor = new Color(0,0,0);
-                } else {
-                    joinColor = new Color(0,0,0);
-                    joinTextColor = new Color(255,255,255);
-                }
-            }
 
             public void paint(Graphics g) {
                 //draw background here
@@ -63,21 +43,61 @@ public class Big10Brawl {
                     startTextColor = new Color(255,255,255);
                 }
                 g.setColor(startColor);
-                g.fillRect(0,0,100,50);
+                g.fillRect(477,646,276,111);
                 if (joinColor == null) {
                     joinColor = new Color(0,0,0);
                     joinTextColor = new Color(255, 255, 255);
-
                 }
                 g.setColor(joinColor);
-                g.fillRect(0,0,100,50);
-
-
+                g.fillRect(797,646,276,111);
+                g.setFont(new Font(Font.SERIF,Font.BOLD,40));
                 g.setColor(startTextColor);
-                g.drawString("Start", 0, 0);
-
+                g.drawString("Start", 570, 710);
                 g.setColor(joinTextColor);
-                g.drawString("Join", 0, 0);
+                g.drawString("Join", 895, 710);
+
+            }
+        };
+    }
+
+    public void initFrame() {
+        welcome.setSize(1550,838);
+        welcome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        welcome.setTitle("Big Ten Brawl");
+        welcome.add(welComp);
+        welcome.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getX() >= 477 && e.getX() <= 753 && e.getY() >= 646 && e.getY() <= 757) {
+                    changeStartColor();
+                } else if (e.getX() >= 797 && e.getX() <= 1073 && e.getY() >= 646 && e.getY() <= 757) {
+                    changeJoinColor();
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.getX() >= 477 && e.getX() <= 753 && e.getY() >= 646 && e.getY() <= 757) {
+                    changeStartColor();
+                    startWaiting();
+                } else if (e.getX() >= 797 && e.getX() <= 1073 && e.getY() >= 646 && e.getY() <= 757) {
+                    changeJoinColor();
+                    join();
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
 
             }
         });
@@ -85,11 +105,38 @@ public class Big10Brawl {
 
     }
 
+    public void changeStartColor() {
+        if (startColor.equals(new Color(0,0,0))) {
+            startColor = new Color(255,255,255);
+            startTextColor = new Color(0,0,0);
+        } else {
+            startColor = new Color(0,0,0);
+            startTextColor = new Color(255,255,255);
+        }
+        welcome.repaint();
+    }
+
+    public void changeJoinColor() {
+        if (joinColor.equals(new Color(0,0,0))) {
+            joinColor = new Color(255,255,255);
+            joinTextColor = new Color(0,0,0);
+        } else {
+            joinColor = new Color(0,0,0);
+            joinTextColor = new Color(255,255,255);
+        }
+        welcome.repaint();
+    }
+
     public void startWaiting() {
+        welcome.setVisible(false);
         waiting = new JFrame();
         waiting.setSize(1550,838);
         waiting.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         waiting.setTitle("Big Ten Brawl");
-        welcome.setVisible(true);
+        waiting.setVisible(true);
+    }
+
+    public void join() {
+        String ip = JOptionPane.showInputDialog("Enter IP of other player");
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,13 @@ public class Player {
     private int charge;
     private int points;
 
+    public final static int PURDUE_PETE = 0;
+    public final static int HERKY_THE_HAWKEYE = 1;
+    public final static int BUCKY_THE_BADGER = 2;
+    public final static int HOOSIER = 3;
+    public final static int BRUTUS_THE_BUCKEYE = 4;
+    public final static int SPARTY_THE_SPARTAN = 5;
+
     public Player() {
         health = 100;
         charge = 100;
