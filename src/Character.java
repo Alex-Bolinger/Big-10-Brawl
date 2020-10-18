e7e7828b752548df803a7e3257b81e266c543c64
ishmistry <imistry@purdue.edu>
2020-10-17 20:25:21 -0400

    Character class given default values in switch statements

--- src/Character.java
+++ src/Character.java
@@ -11,22 +11,12 @@ public class Character {
 
     public static int getSpeed(int character) {
         switch (character) {
+            case  1, 2, 4, 5, 6, 7:
+                return 100;
             case 0:
-                return 0;
-            case 1:
-                return 1;
-            case 2:
-                return 2;
+                return 150;
             case 3:
-                return 3;
-            case 4:
-                return 4;
-            case 5:
-                return 5;
-            case 6:
-                return 6;
-            case 7:
-                return 7;
+                return 1;
             default:
                 System.out.println("Please select a valid character.");
                 return -1;
@@ -36,22 +26,12 @@ public class Character {
     public static int getHealth(int character) {
 
             switch (character) {
+                case 1, 2, 4, 5, 6, 7:
+                    return 100;
                 case 0:
-                    return 0;
-                case 1:
-                    return 1;
-                case 2:
-                    return 2;
+                    return 200;
                 case 3:
-                    return 3;
-                case 4:
-                    return 4;
-                case 5:
-                    return 5;
-                case 6:
-                    return 6;
-                case 7:
-                    return 7;
+                    return 1;
                 default:
                     System.out.println("Please select a valid character.");
                     return -1;
@@ -60,22 +40,8 @@ public class Character {
 
     public static int getCharge(int character) {
         switch (character) {
-            case 0:
-                return 0;
-            case 1:
-                return 1;
-            case 2:
-                return 2;
-            case 3:
-                return 3;
-            case 4:
-                return 4;
-            case 5:
-                return 5;
-            case 6:
-                return 6;
-            case 7:
-                return 7;
+            case 0, 1, 2, 3, 4, 5, 6, 7:
+                return 100;
             default:
                 System.out.println("Please select a valid character.");
                 return -1;
